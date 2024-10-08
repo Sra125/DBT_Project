cdf2959d8ebf1c5ab7802d43d61103c2dae3df6b
sravanchrg@gmail.com <sravanchrg@gmail.com>
2024-10-08 11:53:21 +0000

    semicolon removed from the transform model code

--- models/transform_company_data.sql
+++ models/transform_company_data.sql
@@ -24,6 +24,6 @@ SELECT
     COMPANY_NAME,
     UPPER(COMPANY_NAME) AS TRANSFORMED_COMPANY_NAME, -- New column: transforming company name to uppercase
     CURRENT_DATE AS EXTRACTED_DATE -- New column: capturing the current date as extracted date
-FROM raw_company_data;
+FROM raw_company_data
 
 
